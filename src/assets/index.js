18c54964e3bc4eb45710d2bbf943ecf0affcab29
Hariprasath V <webhari2005@gmail.com>
2024-10-07 20:20:40 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import hari from "./hari.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
-import web from "./web.png";
+import web from "/src/assets/web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
